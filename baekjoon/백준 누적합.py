781d0071ef88ac8b9b12abb7fbb89e44cd9e6e2e
pahtension <90484981+pahtension@users.noreply.github.com>
2025-01-15 17:24:08 +0900

    baekjoon
    주석제거

--- baekjoon/백준 누적합.py
+++ baekjoon/백준 누적합.py
@@ -1,38 +1,34 @@
 
 
-## 2851 마리오
-# p =[]
-# for i in range(10):
-#     p.append(int(input()))
+# 2851 마리오
+p =[]
+for i in range(10):
+    p.append(int(input()))
 
-# n1 = n2 = 0
-# for i in p:
-#     n2+=i
-#     if abs(n2-100) <= abs(n1-100):
-#         n1 = n2
-#     if n2>=100:
-#         break
-# print(n1)
-
-
-## 도박사 곰곰 25199
+n1 = n2 = 0
+for i in p:
+    n2+=i
+    if abs(n2-100) <= abs(n1-100):
+        n1 = n2
+    if n2>=100:
+        break
+print(n1)
 
 
+# 구간 합 11659
+length,n = input().split()
+nl = input().split()
+sl =[]
+for i in range(int(n)):
+    sl.append(input().split())
 
-## 구간 합 11659
-# length,n = input().split()
-# nl = input().split()
-# sl =[]
-# for i in range(int(n)):
-#     sl.append(input().split())
-# #누적합
-# hl =[0]
+hl =[0]
 
-# for i in nl:
-#     hl.append(hl[-1]+int(i))
+for i in nl:
+    hl.append(hl[-1]+int(i))
 
-# for s,e in sl:
-#     print(hl[int(e)]-hl[int(s)-1])
+for s,e in sl:
+    print(hl[int(e)]-hl[int(s)-1])
 
 
 #🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵      -모기 20440
